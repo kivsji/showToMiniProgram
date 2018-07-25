f1338caa3d648702ce5ad3cdc4ef52ecf0b6661c
dongfang <1163129165@qq.com>
2018-07-25 09:51:25 +0800

    commit

--- src/pages/productsType/index.vue
+++ src/pages/productsType/index.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>
-        
+    <div class="prolist">
+        <div class="proSearch">
+            <p class="proSearchText">请搜索。。</p>
+        </div>
     </div>
 </template>
 
@@ -15,5 +17,20 @@
 </script>
 
 <style>
-
+.proSearch{
+    overflow: hidden;
+    height: 70px;
+    background: #FF7F7F;
+}
+.proSearchText{
+    padding-left: 20rpx;
+    color: #fff;
+    line-height: 90px;
+    font-size: 30rpx;
+}
+.prolist{
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+}
 </style>
